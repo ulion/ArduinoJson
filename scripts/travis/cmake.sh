f98da089e794900c7021301c37cda149c4fcc7f6
Benoit Blanchon <github@benoitblanchon.fr>
2016-02-16 21:41:18 +0100

    script

--- scripts/travis/cmake.sh
+++ scripts/travis/cmake.sh
@@ -2,6 +2,6 @@
 
 curl -sS https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz | tar xz -C /tmp --strip 1
 
-/tmp/bin/cmake -DCMAKE_CXX_COMPILER=clang++-$VERSION .
+/tmp/bin/cmake .
 make 
 make test
