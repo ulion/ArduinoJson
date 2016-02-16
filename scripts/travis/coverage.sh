a22d9f7aba322566e3f6fbd5b636638be42a5134
Benoit Blanchon <github@benoitblanchon.fr>
2016-02-16 21:32:52 +0100

    matrix

--- scripts/travis/coverage.sh
+++ scripts/travis/coverage.sh
@@ -2,6 +2,9 @@
 
 curl https://cmake.org/files/v3.4/cmake-3.4.0-Linux-x86_64.tar.gz | tar xz -C /tmp --strip 1
 
-/tmp/bin/cmake -DCMAKE_CXX_COMPILER=g++-$VERSION.
+/tmp/bin/cmake -DCOVERAGE=true .
 make 
 make test
+
+pip install --user cpp-coveralls
+coveralls --exclude third-party --gcov-options '\-lp'; fi
